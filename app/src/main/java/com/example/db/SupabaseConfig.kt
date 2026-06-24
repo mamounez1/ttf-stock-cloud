05d303d3cdfb67025e7f592c57f1ec4625278ed5
mamounez1 <154372545+mamounez1@users.noreply.github.com>
2026-06-24 15:10:51 +0100

    Update SupabaseConfig with new URL and keys
    Added additional constants for Supabase configuration.

--- app/src/main/java/com/example/db/SupabaseConfig.kt
+++ app/src/main/java/com/example/db/SupabaseConfig.kt
@@ -1,6 +1,11 @@
 package com.example.db
 
 object SupabaseConfig {
-    const val URL = "https://vraahrlkxltzixmvxrbe.supabase.co/rest/v1/"
-    const val KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYWFocmxreGx0eml4bXZ4cmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyODkxMDksImV4cCI6MjA5Nzg2NTEwOX0.Asb0dw51e0cSzUGS4YWugQSb9nx_i3lzYQaePWGrdBA"
+    // حطينا كاع الاحتمالات ديال السميات باش البروجي يدوز ناضي
+    const val URL = "https://vraahrlkxltzixmvxrbe.supabase.co/rest/v1/ " 
+    const val SUPABASE_URL = "https://vraahrlkxltzixmvxrbe.supabase.co/rest/v1/ " 
+    
+    const val KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYWFocmxreGx0eml4bXZ4cmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyODkxMDksImV4cCI6MjA5Nzg2NTEwOX0.Asb0dw51e0cSzUGS4YWugQSb9nx_i3lzYQaePWGrdBA"            
+    const val ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYWFocmxreGx0eml4bXZ4cmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyODkxMDksImV4cCI6MjA5Nzg2NTEwOX0.Asb0dw51e0cSzUGS4YWugQSb9nx_i3lzYQaePWGrdBA"       
+    const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYWFocmxreGx0eml4bXZ4cmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyODkxMDksImV4cCI6MjA5Nzg2NTEwOX0.Asb0dw51e0cSzUGS4YWugQSb9nx_i3lzYQaePWGrdBA"   
 }
