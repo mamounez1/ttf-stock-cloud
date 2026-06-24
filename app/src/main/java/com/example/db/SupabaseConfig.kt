ec991484106ed24a933af4f2979469d58b3e065f
mamounez1 <154372545+mamounez1@users.noreply.github.com>
2026-06-24 14:59:57 +0100

    Add Supabase configuration object

---
+++ app/src/main/java/com/example/db/SupabaseConfig.kt
@@ -0,0 +1,6 @@
+package com.example.db
+
+object SupabaseConfig {
+    const val URL = "https://vraahrlkxltzixmvxrbe.supabase.co/rest/v1/"
+    const val KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyYWFocmxreGx0eml4bXZ4cmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyODkxMDksImV4cCI6MjA5Nzg2NTEwOX0.Asb0dw51e0cSzUGS4YWugQSb9nx_i3lzYQaePWGrdBA"
+}
